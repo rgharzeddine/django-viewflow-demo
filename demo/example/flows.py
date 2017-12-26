2b8f800a31d3e673c3ab555d81eeb59d7c36954c
Rawad Gharzeddine <rawad.gharzeddine@rezayat.net>
2017-12-26 11:46:15 +0200

    celery job succeeded

--- demo/example/flows.py
+++ demo/example/flows.py
@@ -20,38 +20,9 @@ from .views import (
 from .tasks import calculate_sheet_payroll
 
 
-def dummy(*args):
-    print('SUCCESS: dummy called')
-
-
 class CeleryJobActivation(AbstractJobActivation):
 
-    def run_async(self, *args, **kwargs):
-        dummy()
-
-        # IMPORTANT
-        # using id(self) as a fast solution instead of
-        # pickling for serialization
-
-        # FAILS TO WORK
-        calculate_sheet_payroll.delay(id(self))
-
-        # WORKS
-        # calculate_sheet_payroll(id(self))
-
-        self.done()
-
     status = AbstractJobActivation.status
-
-    # @status.transition(source=STATUS.ASSIGNED)
-    # def schedule(self):
-    #     print('scheduled !!')
-
-    #     # self.flow_task.job(id(self))
-    #     # self.flow_task.job.delay(id(self))
-    #     # calculate_sheet_payroll.delay(id(self))
-    #     print('schedule finished !!')
-    #     self.done()
 
     @status.transition(
         source=[STATUS.STARTED, STATUS.ASSIGNED], target=STATUS.DONE)
@@ -63,8 +34,16 @@ class CeleryJobActivation(AbstractJobActivation):
         self.activate_next()
 
 
-class FlowJob(flow.AbstractJob):
-    activation_class = CeleryJobActivation
+class PayrollJobActivation(CeleryJobActivation):
+
+    def run_async(self, *args, **kwargs):
+        # WORKS
+        calculate_sheet_payroll.delay(self.process.sheet.id)
+        self.done()
+
+
+class CeleryJob(flow.AbstractJob):
+    pass
 
 
 def assign_user(activation):
@@ -100,11 +79,9 @@ class DailyTimesheetApprovalFlow(Flow):
     )
 
     calculate_payroll = (
-        # flow.Function(
-        FlowJob(
-            # calculate_sheet_payroll
-            # test_celery
-            calculate_sheet_payroll
+        CeleryJob(
+            calculate_sheet_payroll,
+            activation_class=PayrollJobActivation
         )
         .Next(this.approve)
     )
--- demo/example/tasks.py
+++ demo/example/tasks.py
@@ -1,22 +1,25 @@
 """tasks module"""
 # from demo.celery import app
 from celery.decorators import task
-import ctypes
 
-
-def get_by_id(object_id):
-    # return ctypes.cast(object_id, ctypes.py_object).value
-    value = ctypes.cast(object_id, ctypes.py_object).value
-    # print(value)
-    return value
-
-
-# @app.task(bind=True)
+from demo.example.models import DailyTimesheet
 
 
 @task(name='calculate_sheet_payroll')
-def calculate_sheet_payroll(activation_id, *args, **kwargs):
+def calculate_sheet_payroll(sheet_id, *args, **kwargs):
     """calculates the sheet payroll asynchronously"""
-    print('celery task called')
-    activation = ctypes.cast(activation_id, ctypes.py_object).value
-    activation.process.sheet.calculate_payroll()
+    print('celery task called: %s' % sheet_id)
+    done = False
+    counter = 1
+
+    # sometimes for an unkown reason yet:
+    # DailyTimesheet.objects.get(id=int(sheet_id)) raises DoesNotExist
+    while not done:
+        try:
+            sheet = DailyTimesheet.objects.get(id=int(sheet_id))
+            sheet.calculate_payroll()
+            done = True
+            print('tried %s time(s)' % counter)
+        except:
+            counter += 1
+            pass
